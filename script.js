0d9d6a9eab33adbf7c535c9c6dd9ee57ce37af88
wanjirumuigai <92147593+wanjirumuigai@users.noreply.github.com>
2023-01-17 19:17:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 let nav = document.querySelector('#navbar')
 //fetch data from the api
-fetch('https://hp-api.onrender.com/api/characters')
+fetch('https://api.npoint.io/691a24ca2c1fc0e30ed1')
 .then(res => res.json())
 .then(data => {
     displayFirstItem(data[0])
