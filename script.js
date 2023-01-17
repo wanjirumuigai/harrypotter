b6c54d6ce9131bff263c6fb78a4f15894d61f460
Pauline Muigai <wanjirumuigai@gmail.com>
2023-01-17 21:21:00 +0300

    persistent comments

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 let nav = document.querySelector('#navbar')
+let url = 'http://localhost:3000/characters'
 //fetch data from the api
-fetch('https://hp-api.onrender.com/api/characters')
+fetch(url)
 .then(res => res.json())
 .then(data => {
     displayFirstItem(data[0])
@@ -19,6 +20,8 @@ document.getElementById('house').innerHTML = `House: ${characterObj.house}`
 document.getElementById('birthday').innerHTML =  `DOB: ${characterObj.dateOfBirth}`
 document.getElementById('ancestry').innerHTML = `Ancestry: ${characterObj.ancestry}`
 document.getElementById('species').innerHTML = `Species: ${characterObj.species}`
+document.getElementById('name').dataset.id = characterObj.id
+let id = document.getElementById('name').dataset.id
 
 let house = characterObj.house
 let body = document.querySelector('body')
@@ -49,6 +52,7 @@ if(status == true) {
 }
 document.getElementById('status').innerHTML = `Status: ${status}`
 
+  showComments(characterObj.comments)  
 checkLikes()
 
 }
@@ -93,13 +97,9 @@ function showLikes() {
     countArray = []
   
 let likes = document.getElementById('likes')
-console.log(countArray)
-
 
 likes.addEventListener('click', () => {
     countArray.push(1)
-console.log(countArray)
-
 document.querySelector('#likes').classList.add('activated-heart')
 
 document.querySelector('#likes-count').textContent = `${countArray.length} likes`
@@ -110,32 +110,63 @@ document.querySelector('#likes-count').textContent = `${countArray.length} likes
 
 
 // add comments
-
 let form = document.querySelector('#comments-form')
-let comment = document.querySelector('#comments-area')
+form.addEventListener('submit', (e) => {
+    e.preventDefault() 
+    getComments()
+})
 let postedComments = document.getElementById('user-comments')
 postedComments.innerHTML=''
-let li = document.createElement('li')
-
-const addContent = addReview => {
-    const html = `<li>${addReview}</li>`;
-    postedComments.innerHTML += html }
+//get an array of comments posted
+function getComments() {
  
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
+let comment = document.querySelector('#comments-area')
+let retrievedComments
+
+let characterId = document.getElementById('name').dataset.id
+
     let toAdd = comment.value.toUpperCase()
-    li.innerHTML = toAdd
-    addContent(toAdd)
-    form.reset()
+    
+    fetch(`${url}/${characterId}`)
+.then(res => res.json())
+.then(data => {
+    retrievedComments = data.comments
+    retrievedComments.push(toAdd)
+    fetch(`${url}/${characterId}`,
+    {
+      method: 'PATCH',
+      headers: {
+        'Content-Type':'application/json',
+        'Accept': 'application/json'  },
+        body: JSON.stringify({
+          comments: retrievedComments
+        })
+    } )
+    .then(res => res.json())
+    .then(data => data)
+    showComments(retrievedComments) 
+})
+form.reset()
 }
 
-)
+//show comments
+function showComments(comments) {
+        document.getElementById('user-comments').innerHTML=''
+     for (let i=0; i<comments.length; i++) {
+        let li = document.createElement('li')
+        li.innerHTML = comments[i]
+        let list = document.querySelector('#user-comments')
+        list.appendChild(li)
+      }
+   }
+//delete comments
 postedComments.addEventListener('click', function(e) {
     this.removeChild(e.target);
   });
   let list = document.querySelector('#list')
   let search = document.querySelector('.search-bar input')
   
+  //search button
   search.addEventListener('keyup', () => {
   
       const term = search.value.trim().toLowerCase()
