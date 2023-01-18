5d49704000dd4d6ff391ad61d909f3b1d100133d
Charles Swaleh <charlswall@live.com>
2023-01-18 20:58:07 +0300

    Fixed the likes function

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ fetch(url)
 	.then((data) => {
 		displayFirstItem(data[0]);
 		renderCharacters(data);
-		showLikes(data[0]);
+		// handleLikes(data[0]);
 		showComments(data[0].comments);
 	})
 	.catch((error) => console.log("Error: ", error.message));
@@ -23,27 +23,40 @@ function displayFirstItem(characterObj) {
 	document.getElementById("name").dataset.id = characterObj.id;
 	let id = document.getElementById("name").dataset.id;
 
+
+  // Add the likes
+  let heartIcon = document.getElementById("likes");
+	let likes = document.getElementById("likes-count");
+  heartIcon.dataset.count = characterObj.likes
+	likes.innerHTML = `${characterObj.likes} likes`;
+	if (parseInt(likes.textContent) > 0) {
+		heartIcon.classList.add("activated-heart");
+	} else {
+		heartIcon.classList.remove("activated-heart");
+	}
+
+
 	let house = characterObj.house;
 	let body = document.querySelector("body");
-    switch (house) {
-        case "Gryffindor":
-        body.style.backgroundColor = "#740001";
-		nav.style.backgroundColor = "#D3A625";
-        break;
-        case "Slytherin":
-            body.style.backgroundColor = "#1A472A";
-	        nav.style.backgroundColor = "#5D5D5D";
-            break;
-        case "Hufflepuff":
-            body.style.backgroundColor = "#000";
-		    nav.style.backgroundColor = "#FFD800";
-            break;
-        case "Ravenclaw":
-            body.style.backgroundColor = "#0E1A40";
-		    nav.style.backgroundColor = "#946B2D";
-            break;
-    }
-	
+	switch (house) {
+		case "Gryffindor":
+			body.style.backgroundColor = "#740001";
+			nav.style.backgroundColor = "#D3A625";
+			break;
+		case "Slytherin":
+			body.style.backgroundColor = "#1A472A";
+			nav.style.backgroundColor = "#5D5D5D";
+			break;
+		case "Hufflepuff":
+			body.style.backgroundColor = "#000";
+			nav.style.backgroundColor = "#FFD800";
+			break;
+		case "Ravenclaw":
+			body.style.backgroundColor = "#0E1A40";
+			nav.style.backgroundColor = "#946B2D";
+			break;
+	}
+
 	let status = characterObj.alive;
 	if (status == true) {
 		status = "Alive";
@@ -64,37 +77,29 @@ function renderCharacters(characters) {
 		list.appendChild(li);
 		li.addEventListener("click", () => {
 			displayFirstItem(characters[i]);
-			showLikes(characters[i]);
+			// handleLikes(characters[i]);
 			showComments(characters[i].comments);
 		});
 	}
 }
 
-function showLikes(characterObj) {
-	let heartIcon = document.getElementById("likes");
-	let count = characterObj.likes;
-	let likes = document.getElementById("likes-count");
-	let characterId = document.getElementById("name").dataset.id;
-	likes.innerHTML = `${count} likes`;
-	if (count > 0) {
-		heartIcon.classList.add("activated-heart");
-	} else {
-		heartIcon.classList.remove("activated-heart");
-	}
-	heartIcon.addEventListener("click", () => {
-		count += 1;
-		fetch(`${url}/${characterId}`, {
-			method: "PATCH",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				likes: count,
-			}),
-		})
-			.then((res) => res.json())
-			.then((data) => data);
-	});
+
+// Add likes to the DOM and DB
+document.getElementById("likes").addEventListener("click", handleLikes);
+function handleLikes(e) {
+  const currentLikes = parseInt(e.target.dataset.count)
+  let characterId = document.getElementById("name").dataset.id
+  fetch(`${url}/${characterId}`, {
+  	method: "PATCH",
+  	headers: {
+  		"Content-Type": "application/json",
+  	},
+  	body: JSON.stringify({
+  		likes: currentLikes + 1,
+  	}),
+  })
+  	.then((res) => res.json())
+  	.then((data) => data);
 }
 
 // add comments
